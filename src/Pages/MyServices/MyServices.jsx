b8feeae8413fc2a062b3c275d354214383da10ba
Abdur Rahman <abdur1222.ar@gmail.com>
2023-11-08 07:15:13 +0600

    used jwt in my service page to show the products

--- src/Pages/MyServices/MyServices.jsx
+++ src/Pages/MyServices/MyServices.jsx
@@ -1,10 +1,23 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+import axios from "axios";
 
 const MyServices = () => {
-    return (
-        <div>
-            <h2>My Service</h2>
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  const [products, setProducts] = useState([]);
+
+  const url = `https://go-car-service-server.vercel.app/products?email=${user?.email}`;
+  useEffect(() => {
+    axios.get(url, { withCredentials: true }).then((res) => {
+      setProducts(res.data);
+    });
+  }, [url]);
+  return (
+    <div className="my-10">
+      <h2>My Service</h2>
+      <h2 className="text-5xl">Your bookings: {products.length}</h2>
+    </div>
+  );
 };
 
 export default MyServices;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -8,46 +8,62 @@ import Services from "../Pages/Services/Services";
 import PrivateRoute from "./PrivateRoute";
 import ServiceDetails from "../Pages/ServiceDetails/ServiceDetails";
 import AddServices from "../Pages/AddServices/AddServices";
-
+import MyServices from "../Pages/MyServices/MyServices";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root></Root>,
-      errorElement: <ErrorPage></ErrorPage>,
-      loader: () => fetch("https://go-car-service-server.vercel.app"),
-      children: [
-        {
-          path: "/",
-          element: <Home></Home>,
-        },
-        {
-          path: "/login",
-          element: <Login></Login>,
-        },
-        {
-          path: "/register",
-          element: <Register></Register>,
-        },
-        {
-          path: "/services",
-          element: <Services></Services>,
-        },
-        {
-          path: "/servicedetails/:id",
-          element: <PrivateRoute>
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    loader: () => fetch("https://go-car-service-server.vercel.app"),
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/services",
+        element: <Services></Services>,
+      },
+      {
+        path: "/servicedetails/:id",
+        element: (
+          <PrivateRoute>
             <ServiceDetails></ServiceDetails>
-          </PrivateRoute>,
-                  loader: ({ params }) => fetch(`https://go-car-service-server.vercel.app/products/${params.id}`),
-        },
-        {
-          path: "/addservices",
-          element: <PrivateRoute>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(
+            `https://go-car-service-server.vercel.app/products/${params.id}`
+          ),
+      },
+      {
+        path: "/addservices",
+        element: (
+          <PrivateRoute>
             <AddServices></AddServices>
           </PrivateRoute>
-        }
-      ]
-    }
+        ),
+        loader: () => fetch("https://go-car-service-server.vercel.app/products"),
+      },
+      {
+        path: "/myservices",
+        element: (
+          <PrivateRoute>
+            <MyServices></MyServices>
+          </PrivateRoute>
+        )
+      },
+    ],
+  },
 ]);
 
 export default router;
